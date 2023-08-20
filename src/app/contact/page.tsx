9918347341fe1d9c887f4a86520a39208a93c412
gastonfr24 <gastonfr24@gmail.com>
2023-08-20 17:34:08 -0300

    simple SEO

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,13 @@
 import Header from '@/components/contact/Header'
 import style from './page.module.css'
 
+
+export const metadata = {
+  title: 'Gastón Franco | Contacto',
+  description: 'Enviar una cotización para recibir precios sobre mis servicios de desarrollo web y marketing',
+  keywords: "web design, blog, ecommerce, portfolio, marketing, pricing, price, precio, product, productos",
+}
+
 function page() {
   return (
     <div
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,6 +2,12 @@ import Services from '@/components/home/services/Services'
 import Header from '@/components/services/header/Header'
 import React from 'react'
 
+export const metadata = {
+  title: 'Gastón Franco | Servicios',
+  description: 'Echa un vistazo a mis servicios de desarrollo web, marketing, diseño web y más.',
+  keywords: "web design, blog, ecommerce, portfolio, diseño web, desarrollo web, web development",
+}
+
 function page() {
   return (
     <div className='bg-graydark px-10 lg:px-24'>
